be090916137d7a584510fa2af9dec618be212e9f
Jeff Sarpong <crazicoda@gmail.com>
2022-02-15 20:09:24 +0000

    Updates

--- Damsel/src/Damsel/Logger.cpp
+++ Damsel/src/Damsel/Logger.cpp
@@ -1,13 +1,13 @@
 #include "Logger.h"
 
 namespace Damsel {
-	Logger::Logger(): m_Name("root"), log_level(0) {}
+	Logger::Logger() : m_Name("root"), log_level(0) { getCurrentTime(); }
 
-	Logger::Logger(const std::string& name): m_Name(name), log_level(0) {
-	}
-	Logger::~Logger(){}
+	Logger::Logger(const std::string& name) : m_Name(name), log_level(0) { getCurrentTime(); }
+	Logger::~Logger() {}
 
-	void Logger::setup(const std::string& file, const std::string& encoding, Logger::Level level){
+	void Logger::setup(const std::string& file, const std::string& encoding, Logger::Level level)
+	{
 		log_level = int(level);
 		log_path = file;
 		encoding_type = encoding;
@@ -20,33 +20,113 @@ namespace Damsel {
 
 	int Logger::getLogLevel() { return log_level; }
 
-	void Logger::info(char* message){
-		Logger::Level level { Logger::Level::INFO };
+	void Logger::info(char* message)
+	{
+		Logger::Level level{ Logger::Level::INFO };
 		this->log(message, int(level));
 	}
 
-	void Logger::warning(char* message) {
+	void Logger::warning(char* message)
+	{
 		Logger::Level level{ Logger::Level::WARNING };
 		this->log(message, int(level));
 	}
 
-	void Logger::error(char* message) {
+	void Logger::error(char* message)
+	{
 		Logger::Level level{ Logger::Level::ERROR };
 		this->log(message, int(level));
 	}
 
-	void Logger::critical(char* message) {
+	void Logger::critical(char* message)
+	{
 		Logger::Level level{ Logger::Level::CRITICAL };
 		this->log(message, int(level));
 	}
 
-	void Logger::fatal(char* message) {
+	void Logger::fatal(char* message)
+	{
 		Logger::Level level{ Logger::Level::FATAL };
 		this->log(message, int(level));
 	}
 
-	void Logger::log(char* message, int level){
-		std::cout << level << " " << message << std::endl;
+	void Logger::log(char* message, int level)
+	{
+		std::cout << getLogMessage(message) << std::endl;
 	}
 
+	//Time Functions
+	void Logger::getCurrentTime()
+	{
+		time(&currentTime);
+		localTime = localtime(&currentTime);
+	}
+	int Logger::getYear()
+	{
+		getCurrentTime();
+		return 1900 + localTime->tm_year;
+	}
+	int Logger::getMonth()
+	{
+		getCurrentTime();
+		return localTime->tm_mon + 1;
+	}
+	int Logger::getDay()
+	{
+		getCurrentTime();
+		return localTime->tm_mday;
+	}
+	int Logger::getHour()
+	{
+		getCurrentTime();
+		return localTime->tm_hour;
+	}
+	int Logger::getMinute()
+	{
+		getCurrentTime();
+		return localTime->tm_min;
+	}
+	int Logger::getSecond()
+	{
+		getCurrentTime();
+		return localTime->tm_sec;
+	}
+
+	// Set Pattern
+
+
+	void Logger::setPattern(char* pattern) { this->pattern = pattern; }
+
+	const std::string Logger::getLogMessage(char* message) {
+		std::string new_message = "";
+		for (int i = 0; i < pattern.size(); i++)
+		{
+			if (int(pattern[i]) == 37) {
+				int char_code = pattern[i + 1];
+				new_message += patternCommand(char_code, message);
+				i++;
+			}
+			else {
+				new_message += pattern[i];
+			}
+		}
+		return new_message;
+	}
+
+	std::string Logger::patternCommand(int char_code, char* message) {
+		switch (char_code) {
+		case 72:
+			return std::to_string(getHour());
+		case 77:
+			return std::to_string(getMinute());
+		case 83:
+			return std::to_string(getMinute());
+		case 110:
+			return m_Name;
+		case 109:
+			return message;
+		default:
+			return "";
+		}
+	}
 }
--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -1,7 +1,7 @@
 #include "Damsel.h"
 
 int main() {
-	Damsel::Logger logger = Damsel::Logger("main");
+	Damsel::Logger logger = Damsel::Logger("SandBox");
 	logger.setup("logs.log", "utf-8", Damsel::Logger::Level::INFO);
 	logger.fatal("Hello world");
 	return 0;
